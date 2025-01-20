26bb3a7a3bd2070892e26b2637f00220c5948b8f
Ben Meadors <benmmeadors@gmail.com>
2025-01-20 13:09:03 -0600

    Revert "feat: added tailwind css to project (#16)"
    This reverts commit 0fd2c50288e3d19e8cb2c484a39d7aacf401ca60.

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	darkMode: ["class", '[data-theme="dark"]'],
-	content: ["./index.html", "./src/**/*.{ts,tsx}"],
-	theme: {},
-	plugins: [require("tailwindcss-animate")],
-};
